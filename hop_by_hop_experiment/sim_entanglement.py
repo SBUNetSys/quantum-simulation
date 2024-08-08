762a81ec3eb7ffaf001ccb931a4cdb4a72318717
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2024-08-08 13:06:06 -0400

    added hop by hop experiment

--- hop_by_hop_experiment/sim_entanglement.py
+++ hop_by_hop_experiment/sim_entanglement.py
@@ -26,6 +26,11 @@ from entangle import GenEntanglement
 from messagehandler import MessageHandler
 from manage_entangle import ManageEntanglement
 
+plt.rcParams['axes.labelsize'] = 16
+plt.rcParams['axes.titlesize'] = 18
+plt.rcParams['xtick.labelsize'] = 14
+plt.rcParams['ytick.labelsize'] = 14
+plt.rcParams['legend.fontsize'] = 14
 
 def print_green(text):
     print(f"\033[92m{text}\033[0m")
@@ -171,6 +176,35 @@ def example_sim_run(nodes, num_runs, memory_depolar_rate, node_distance, max_ent
     return entangle_example, dc
 
 
+def plot_lines(xs, ys, title, x_label, y_label, data_legends, xlim=None, save=True, save_dir="./", num_bins=20):
+    fig, ax = plt.subplots(figsize=(12, 6))
+    for x, y, legend in zip(xs, ys, data_legends):
+        ax.plot(x, y, label=f'{legend}', lw=2)
+    ax.set_title(f'{title}')
+    ax.set_xlabel(f'{x_label}')
+    ax.set_ylabel(f'{y_label}')
+    ax.tick_params(axis='x', labelrotation=90)
+    ax.legend(loc="best")  # loc="upper left"bbox_to_anchor=(1.05, 1)
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    # rotate x labels
+    plt.xticks(rotation=45)
+    ax.set_xlim(left=0)
+    ax.set_ylim(top=1)
+    if xlim:
+        ax.set_xlim(xlim)
+    # Group x labels
+    ax.xaxis.set_major_locator(plt.MaxNLocator(integer=True))
+    ax.xaxis.set_major_locator(plt.MaxNLocator(nbins=20))
+    # ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x)}'))  # Format the labels as integers
+    plt.tight_layout()
+    if save:
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        plt.savefig(os.path.join(save_dir, f"{title}.png"))
+    plt.show()
+
+
 def experiment_with_increasing_nodes(max_node, save_dir):
     # create a network
     nodes_list = [f"Node_{i}" for i in range(max_node)]
@@ -217,7 +251,19 @@ def experiment_with_increasing_nodes(max_node, save_dir):
 
 
 def main():
-    experiment_with_increasing_nodes(50, "./")
+    # experiment_with_increasing_nodes(50, "./")
+    with open("entanglement_results_50_node.json", "r") as f:
+        data = json.load(f)
+    xs = list(int(key) for key in data.keys())
+    ys = [[data[key]["actual_fidelity"] for key in data.keys()],
+          [data[key]["estimated_fidelity"] for key in data.keys()]]
+    with open("../swapping_experiment/fidelity_data_max_node_50.json", "r") as f:
+        data = json.load(f)
+    ys.append(list(np.mean(data[key]) for key in data.keys()))
+    plot_lines([xs, xs, xs[1:]], ys, "Entanglement fidelity vs number of nodes",
+               "Number of nodes", "Fidelity",
+               ["Hop-by-Hop Actual fidelity","Hop-by-Hop Estimated fidelity", "Swapping Actual fidelity"],
+               save=True, save_dir="./")
 
 
 if __name__ == '__main__':
