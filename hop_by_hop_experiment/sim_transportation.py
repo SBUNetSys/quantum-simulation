c9009bb3263d030c96f0e1bddd5a86f9e77d607f
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2025-01-27 17:02:31 -0500

    - bug fix verification throughput

--- hop_by_hop_experiment/sim_transportation.py
+++ hop_by_hop_experiment/sim_transportation.py
@@ -470,6 +470,7 @@ class TransportWithVerificationThroughput(LocalProtocol):
                                         MessageType.TRANSPORT_SUCCESS)
                 results = self.subprotocols[f"transport_{self.all_nodes[-1].name}"].get_signal_result(
                     MessageType.TRANSPORT_SUCCESS, self)
+                print(results)
                 self.send_signal(Signals.SUCCESS, results)
 
 
