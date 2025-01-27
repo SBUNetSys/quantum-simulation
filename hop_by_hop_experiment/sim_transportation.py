5fc15a03c34bfb526a9ca39ebf1c05d3b8c82f76
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2025-01-27 16:57:04 -0500

    - add verification throughput

--- hop_by_hop_experiment/sim_transportation.py
+++ hop_by_hop_experiment/sim_transportation.py
@@ -282,6 +282,207 @@ class TransportWithVerificationExample(LocalProtocol):
         for subprotocol in self.subprotocols.values():
             subprotocol.stop()
 
+class TransportWithVerificationThroughput(LocalProtocol):
+    """
+    Protocol for a complete verification example.
+    """
+
+    def __init__(self, network_nodes,
+                 num_runs=1,
+                 max_entangle_pairs=2,
+                 memory_depolar_rate=1,
+                 node_distance=20,
+                 target_fidelity=0.99,
+                 m_size=3,
+                 batch_size=10,
+                 qubits_to_transport=1,
+                 skip_noise=False,
+                 CU_gate = None,
+                 CCU_gate = None,):
+        if len(network_nodes) < 1:
+            raise ValueError("This protocol requires at least nodes.")
+        self.all_nodes = network_nodes
+        self.num_runs = num_runs
+        self.max_entangle_pairs = max_entangle_pairs
+        self.m_size = m_size
+        self.batch_size = batch_size
+        self.qubits_to_transport = qubits_to_transport
+        super().__init__(nodes={node.name: node for node in network_nodes}, name="ExampleTransportation")
+        # create logger
+        self.logger = Logging.Logger(self.name, logging_enabled=False)
+        null_logger = Logging.Logger("null", logging_enabled=False)
+        self.skip_noise = skip_noise
+
+        # Initialize the controlled unitary matrix and measurement operators
+        # CU_matrix = controlled_unitary(batch_size)
+        measurement_m0, measurement_m1 = measure_operator()
+        # CU_gate = ops.Operator("CU_Gate", CU_matrix)
+        # CCU_gate = CU_gate.conj
+
+        # initialize the protocol for each node
+        for index, node in enumerate(network_nodes):
+            # Initialize the MessageHandler protocol
+            self.add_subprotocol(MessageHandler(node=node,
+                                                name=f"message_handler_{node.name}",
+                                                cc_ports=self.get_cc_ports(node)
+                                                ))
+            qubit_input_protocols = []
+            # Initialize the GenEntanglement protocol and EntanglementHandler protocol
+            if index - 1 >= 0:
+                # case of we have a previous node
+                gen_protocol = GenEntanglement(
+                    input_mem_pos=0,
+                    total_pairs=self.max_entangle_pairs,
+                    entangle_node=network_nodes[index - 1].name,
+                    node=node,
+                    name=f"entangle_{node.name}->{network_nodes[index - 1].name}",
+                    is_source=False,
+                    logger=null_logger
+                )
+                self.add_subprotocol(gen_protocol)
+                eh_handler = EntanglementHandler(node=node,
+                                                 name=f"entanglement_handler_{node.name}->{network_nodes[index - 1].name}",
+                                                 num_pairs=self.max_entangle_pairs,
+                                                 qubit_input_protocol=gen_protocol,
+                                                 cc_message_handler=self.subprotocols[
+                                                     f"message_handler_{node.name}"],
+                                                 entangle_node=network_nodes[index - 1].name,
+                                                 memory_depolar_rate=memory_depolar_rate,
+                                                 node_distance=node_distance,
+                                                 is_top_layer=False,
+                                                 logger=null_logger
+                                                 )
+                self.add_subprotocol(eh_handler)
+                gen_protocol.entanglement_handler = eh_handler
+                # add purification
+                pure_protocol = Purification(node=node,
+                                             name=f"purify_{node.name}->{network_nodes[index - 1].name}",
+                                             entangled_node=network_nodes[index - 1].name,
+                                             entanglement_handler=eh_handler,
+                                             cc_message_handler=self.subprotocols[f"message_handler_{node.name}"],
+                                             max_entangled_pair=self.max_entangle_pairs,
+                                             target_fidelity=target_fidelity,
+                                             is_top_layer=False,
+                                             logger=null_logger
+                                             )
+                self.add_subprotocol(pure_protocol)
+                verify_protocol = Verification(node=node,
+                                               name=f"verify_{node.name}->{network_nodes[index - 1].name}",
+                                               entangled_node=network_nodes[index - 1].name,
+                                               purification_protocol=pure_protocol,
+                                               cc_message_handler=self.subprotocols[f"message_handler_{node.name}"],
+                                               m_size=m_size,
+                                               batch_size=batch_size,
+                                               CU_Gate=CU_gate,
+                                               CCU_Gate=CCU_gate,
+                                               measurement_m0=measurement_m0,
+                                               measurement_m1=measurement_m1,
+                                               logger=null_logger,
+                                               is_top_layer=False,
+                                               max_entangled_pairs=self.max_entangle_pairs,
+                                               )
+                self.add_subprotocol(verify_protocol)
+                qubit_input_protocols.append(verify_protocol)
+
+            if index + 1 < len(network_nodes):
+                # case of we have a next node
+                gen_protocol = GenEntanglement(
+                    input_mem_pos=0,
+                    total_pairs=self.max_entangle_pairs,
+                    entangle_node=network_nodes[index + 1].name,
+                    node=node,
+                    name=f"entangle_{node.name}->{network_nodes[index + 1].name}",
+                    is_source=True,
+                    logger=null_logger
+                )
+                self.add_subprotocol(gen_protocol)
+                eh_handler = EntanglementHandler(node=node,
+                                                 name=f"entanglement_handler_{node.name}->{network_nodes[index + 1].name}",
+                                                 num_pairs=self.max_entangle_pairs,
+                                                 qubit_input_protocol=gen_protocol,
+                                                 cc_message_handler=self.subprotocols[
+                                                     f"message_handler_{node.name}"],
+                                                 entangle_node=network_nodes[index + 1].name,
+                                                 memory_depolar_rate=memory_depolar_rate,
+                                                 node_distance=node_distance,
+                                                 is_top_layer=False,
+                                                 logger=null_logger
+                                                 )
+                self.add_subprotocol(eh_handler)
+                gen_protocol.entanglement_handler = eh_handler
+                # Initialize the purification protocol
+                pure_protocol = Purification(node=node,
+                                             name=f"purify_{node.name}->{network_nodes[index + 1].name}",
+                                             entangled_node=network_nodes[index + 1].name,
+                                             entanglement_handler=eh_handler,
+                                             cc_message_handler=self.subprotocols[
+                                                 f"message_handler_{node.name}"],
+                                             max_entangled_pair=self.max_entangle_pairs,
+                                             target_fidelity=target_fidelity,
+                                             is_top_layer=False,
+                                             logger=null_logger
+                                             )
+                self.add_subprotocol(pure_protocol)
+                verify_protocol = Verification(node=node,
+                                               name=f"verify_{node.name}->{network_nodes[index + 1].name}",
+                                               entangled_node=network_nodes[index + 1].name,
+                                               purification_protocol=pure_protocol,
+                                               cc_message_handler=self.subprotocols[f"message_handler_{node.name}"],
+                                               m_size=m_size,
+                                               batch_size=batch_size,
+                                               CU_Gate=CU_gate,
+                                               CCU_Gate=CCU_gate,
+                                               measurement_m0=measurement_m0,
+                                               measurement_m1=measurement_m1,
+                                               logger=null_logger,
+                                               is_top_layer=False,
+                                               max_entangled_pairs=self.max_entangle_pairs,
+                                               )
+                self.add_subprotocol(verify_protocol)
+                qubit_input_protocols.append(verify_protocol)
+            # add transport protocol
+            entangle_name = ""
+            if index + 1 < len(network_nodes):
+                entangle_name = network_nodes[index + 1].name
+            else:
+                entangle_name = network_nodes[index - 1].name
+            transport = Transportation(node=node,
+                                       name=f"transport_{node.name}",
+                                       qubit_ready_protocols=qubit_input_protocols,
+                                       entangled_node=entangle_name,
+                                       source=network_nodes[0].name,
+                                       destination=network_nodes[-1].name,
+                                       cc_message_handler=self.subprotocols[f"message_handler_{node.name}"],
+                                       transmitting_qubit_size=qubits_to_transport,
+                                       logger=self.logger,
+                                       is_top_layer=True,
+                                       )
+            self.add_subprotocol(transport)
+
+    def run(self):
+        self.start_subprotocols()
+        # for subprotoco, val in self.subprotocols.items():
+        #     print(f"Subprotocol: {subprotoco}")
+
+        for index in range(self.num_runs):
+            while True:
+                yield self.await_signal(self.subprotocols[f"transport_{self.all_nodes[-1].name}"],
+                                        MessageType.TRANSPORT_FINISHED)
+                results = self.subprotocols[f"transport_{self.all_nodes[-1].name}"].get_signal_result(
+                    MessageType.TRANSPORT_FINISHED, self)
+                self.send_signal(Signals.SUCCESS, results)
+
+
+    def get_cc_ports(self, node):
+        cc_ports = {}
+        for n in self.all_nodes:
+            if n != node:
+                cc_ports[n.name] = node.get_conn_port(n.ID)
+        return cc_ports
+
+    def stop(self):
+        for subprotocol in self.subprotocols.values():
+            subprotocol.stop()
 
 class TransportWithPurificationExample(LocalProtocol):
     """
@@ -695,6 +896,51 @@ def example_sim_run_with_verification(nodes, num_runs, memory_depolar_rate,
         print(f"Verification Run {result['run_index']} completed, fid{result['results']['teleport_fids']}, "
               f"sim_time {sim_time()}")
         return result["results"]
+
+    dc = DataCollector(record_run, include_time_stamp=False,
+                       include_entity_name=False)
+    dc.collect_on(pd.EventExpression(source=transport_example,
+                                     event_type=Signals.SUCCESS.value))
+    return transport_example, dc
+
+def example_sim_run_with_verification_throughput(nodes, num_runs, memory_depolar_rate,
+                                      node_distance, max_entangle_pairs, target_fidelity, m_size, batch_size,
+                                      qubit_to_transport, CU_gate, CCU_gate,
+                                      skip_noise=True):
+    """
+    Run the example verification protocol
+    :param nodes: list of nodes
+    :param num_runs: number of runs
+    :param memory_depolar_rate: memory depolar rate
+    :param node_distance: node distance
+    :param max_entangle_pairs: maximum entangle pairs
+    :param target_fidelity: target fidelity
+    :param m_size: m size
+    :param batch_size: batch size
+    :param qubit_to_transport: number of qubits to transmit
+    :param skip_noise: skip noise when popping qubits
+    :return:
+    """
+    # Create the protocol
+    transport_example = TransportWithVerificationExample(network_nodes=nodes,
+                                                         num_runs=num_runs,
+                                                         max_entangle_pairs=max_entangle_pairs,
+                                                         memory_depolar_rate=memory_depolar_rate,
+                                                         node_distance=node_distance,
+                                                         target_fidelity=target_fidelity,
+                                                         m_size=m_size,
+                                                         batch_size=batch_size,
+                                                         skip_noise=skip_noise,
+                                                         qubits_to_transport=qubit_to_transport,
+                                                         CU_gate=CU_gate,
+                                                         CCU_gate=CCU_gate,)
+
+    # Run the protocol
+    def record_run(evexpr):
+        protocol = evexpr.triggered_events[-1].source
+        result = protocol.get_signal_result(Signals.SUCCESS)
+        print(f"Transported {len(result['results'])} Qubit")
+        return result
 
     dc = DataCollector(record_run, include_time_stamp=False,
                        include_entity_name=False)
@@ -1306,6 +1552,69 @@ def run_evaluation_4_node_verify_new(qubit_number=1):
         with open(f"./transportation_results/4nodes_{qubit_number}_qubit_verification.json", "w") as f:
             json.dump(final_result, f)
 
+def run_evaluation_4_node_verify_throughput(qubit_number=1000):
+    final_data_raw = {}
+    final_data = {}
+    success_count = []
+    total_count = []
+    average_fids = []
+    CU_matrix = controlled_unitary(4)
+    CU_gate = ops.Operator("CU_Gate", CU_matrix)
+    CCU_gate = CU_gate.conj
+    for i in range(1000):
+        print(f"Run {i}/1000")
+        nodes_list = [f"Node_{i}" for i in range(4)]
+        network = setup_network(nodes_list, "hop-by-hop-verify-transportation",
+                                memory_capacity=1000, memory_depolar_rate=63109,
+                                node_distance=1, source_delay=1)
+        # create a protocol to entangle two nodes
+        sample_nodes = [node for node in network.nodes.values()]
+        transport_example, dc = example_sim_run_with_verification_throughput(sample_nodes, num_runs=1, memory_depolar_rate=63109,
+                                                                  node_distance=1,
+                                                                  max_entangle_pairs=1000, target_fidelity=0.98,
+                                                                  skip_noise=True, qubit_to_transport=qubit_number,
+                                                                  m_size=3, batch_size=4,
+                                                                  CU_gate=CU_gate,
+                                                                  CCU_gate=CCU_gate)
+        # Run the simulation
+        transport_example.start()
+        ns.sim_run(duration=1e9)
+        # Collect the data
+        collected_data = dc.dataframe
+        final_row = collected_data.tail(1)
+        node_data = {
+            "total_count": 0,
+            "teleport_success_count": 0,
+            "average_fidelity": 0,
+        }
+        all_fid = []
+        for c in final_row.columns:
+            data = final_row[c].iloc[0]
+            for fid in data.values():
+                all_fid.append(fid)
+                if fid > 0.99:
+                    node_data["teleport_success_count"] += 1
+                node_data["total_count"] += 1
+        node_data["average_fidelity"] = np.mean(all_fid)
+        final_data_raw[i] = node_data
+        print(f"Finished {i}/1000\n{node_data}")
+        total_count.append(node_data["total_count"])
+        average_fids.append(node_data["average_fidelity"])
+        success_count.append(node_data["teleport_success_count"])
+
+        transport_example.stop()
+        ns.set_random_state(rng=np.random.RandomState())
+        ns.sim_reset()
+
+        final_data["total_count"] = np.mean(total_count)
+        final_data["average_fidelity"] = np.mean(average_fids)
+        final_data["teleport_success_count"] = np.mean(success_count)
+
+        with open(f"./transportation_results/4nodes_verification_throughput_raw.json", "w") as f:
+            json.dump(final_data_raw, f)
+
+        with open(f"./transportation_results/4nodes_verification_throughput.json", "w") as f:
+            json.dump(final_data, f)
 
 if __name__ == '__main__':
     seed = np.random.randint(0, 10000)
@@ -1318,6 +1627,8 @@ if __name__ == '__main__':
             run_evaluation_5_node_throughput(1000)
         if opt == 1:
             run_evaluation_4_node_verify_new(1)
+        if opt == 2:
+            run_evaluation_4_node_verify_throughput(1000)
     else:
         print(" arg 0 = purification_throughput, 1 = verification")
 
