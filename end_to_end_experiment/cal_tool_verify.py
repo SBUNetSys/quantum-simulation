8a7c773fc8bf8d58bc49e45c3083d0d6e0c6fbc6
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2025-01-30 16:35:28 -0500

    - bug fix verification throughput

--- end_to_end_experiment/cal_tool_verify.py
+++ end_to_end_experiment/cal_tool_verify.py
@@ -141,7 +141,7 @@ if __name__ == '__main__':
     g_9_global_count = 0
     total_count = 0
     all_count = []
-    for value_data in data["4"].values():
+    for value_data in data["3"].values():
         g_7_count = 0
         g_9_count = 0
         fid_data = value_data["all_fids"]
