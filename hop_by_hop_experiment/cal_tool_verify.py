2a40c18120bb0e1bdb5d5a77d829c6873734574d
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2025-01-30 14:38:31 -0500

    - bug fix verification throughput

--- hop_by_hop_experiment/cal_tool_verify.py
+++ hop_by_hop_experiment/cal_tool_verify.py
@@ -91,3 +91,36 @@ if __name__ == '__main__':
     print(f"4 Nodes Throughput > 0.9: {np.mean(g_9_counts)}")
     print(f"4 Nodes Global > 0.7: {g_7_global_count / total_count}")
     print(f"4 Nodes Global > 0.9: {g_9_global_count / total_count}")
+
+
+
+    # 0.5 km
+    with open("./transportation_results/4nodes_verification_throughput_batch_4_0.5.json", "r") as file:
+        data = json.load(file)
+    g_7_counts = []
+    g_9_counts = []
+    g_7_global_count = 0
+    g_9_global_count = 0
+    total_count = 0
+    for value_data in data.values():
+        g_7_count = 0
+        g_9_count = 0
+        fid_data = value_data["all_fidelity"]
+        for fid in fid_data:
+            if fid > 0.7:
+                g_7_count += 1
+                g_7_global_count += 1
+            if fid > 0.9:
+                g_9_count += 1
+                g_9_global_count += 1
+            total_count += 1
+        if len(fid_data) > 0:
+            g_7_counts.append(g_7_count)
+            g_9_counts.append(g_9_count)
+
+    print(f"4 Nodes 0.5km Throughput > 0.7: {np.mean(g_7_counts)}")
+    print(f"4 Nodes 0.5km Throughput > 0.9: {np.mean(g_9_counts)}")
+    print(f"4 Nodes 0.5km Global > 0.7: {g_7_global_count / total_count}")
+    print(f"4 Nodes 0.5km Global > 0.9: {g_9_global_count / total_count}")
+
+
