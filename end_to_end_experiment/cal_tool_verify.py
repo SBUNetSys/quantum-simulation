43eaa966cedfbec2b6919a782e728b778891548b
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2025-01-30 16:35:11 -0500

    - bug fix verification throughput

--- end_to_end_experiment/cal_tool_verify.py
+++ end_to_end_experiment/cal_tool_verify.py
@@ -132,3 +132,34 @@ if __name__ == '__main__':
     print(f"4 Nodes 0.5 Km Verify > 0.9: {g_9_count/ len(all_fid)}")
     print(f"average duration: {np.mean(d_data)}")
     print(f"average fid: {np.mean(all_fid)}\n")
+
+    with open("./transportation_results/e2e_3nodes_throughput_1km_verify_raw.json", "r") as file:
+        data = json.load(file)
+    g_7_counts = []
+    g_9_counts = []
+    g_7_global_count = 0
+    g_9_global_count = 0
+    total_count = 0
+    all_count = []
+    for value_data in data["4"].values():
+        g_7_count = 0
+        g_9_count = 0
+        fid_data = value_data["all_fids"]
+        for fid in fid_data:
+            if fid > 0.7:
+                g_7_count += 1
+                g_7_global_count += 1
+            if fid > 0.9:
+                g_9_count += 1
+                g_9_global_count += 1
+            total_count += 1
+        if len(fid_data) > 0:
+            g_7_counts.append(g_7_count)
+            g_9_counts.append(g_9_count)
+        all_count.append(value_data["total_count"])
+
+    print(f"e2e 3 Nodes 1km Throughput Count: {np.mean(all_count)}")
+    print(f"e2e 3 Nodes 1km Throughput > 0.7: {np.mean(g_7_counts)}")
+    print(f"e2e 3 Nodes 1km Throughput > 0.9: {np.mean(g_9_counts)}")
+    print(f"e2e 3 Nodes 1km Global > 0.7: {g_7_global_count / total_count}")
+    print(f"e2e 3 Nodes 1km Global > 0.9: {g_9_global_count / total_count}\n")
