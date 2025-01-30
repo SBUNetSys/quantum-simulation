1ea3320d9e1269dd43b57603549011cdd7908f1f
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2025-01-29 22:42:41 -0500

    - bug fix verification throughput

---
+++ end_to_end_experiment/cal_tool_verify.py
@@ -0,0 +1,27 @@
+import json
+
+import numpy as np
+
+if __name__ == '__main__':
+    with open("./transportation_results/e2e_3nodes_1_qubit_verification_raw.json", "r") as file:
+        data = json.load(file)
+    fid_data = data["teleport_fids"]
+    g_7_count = 0
+    g_9_count = 0
+    all_fid = []
+    d_data = data["duration"]
+    for fid in fid_data:
+        if fid is None:
+            continue
+        if fid > 0.7:
+            g_7_count += 1
+        if fid > 0.9:
+            g_9_count += 1
+        all_fid.append(fid)
+
+    print(f"3 Nodes > 0.7: {g_7_count/ len(all_fid)}")
+    print(f"3 Nodes > 0.9: {g_9_count/ len(all_fid)}")
+    print(f"average duration: {np.mean(d_data)}")
+    print(f"average fid: {np.mean(all_fid)}")
+
+
