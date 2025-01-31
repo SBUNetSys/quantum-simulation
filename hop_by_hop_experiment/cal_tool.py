a2e43d4058545228a6b477651df172438b6739cd
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2025-01-31 17:04:24 -0500

    - data

--- hop_by_hop_experiment/cal_tool.py
+++ hop_by_hop_experiment/cal_tool.py
@@ -3,17 +3,35 @@ import json
 import numpy as np
 
 if __name__ == '__main__':
-    with open("./transportation_results/5nodes_throughput_raw_4_node.json", "r") as file:
-        data = json.load(file)
-    run_data = data["4"]
+    with open("./transportation_results/4nodes_1_qubit_verification_1.0km_24583hz_raw.json", 'r') as f:
+        data = json.load(f)
+    fid_data = data["teleport_fids"]
+    g_7_count = 0
+    g_9_count = 0
+    d_data = data["duration"]
+    for fid in fid_data:
+        if fid > 0.7:
+            g_7_count += 1
+        if fid > 0.9:
+            g_9_count += 1
 
-    avg_count = []
-    avg_fid = []
-    avg_success = []
-    for v in run_data.values():
-        avg_count.append(v["total_count"])
-        avg_fid.append(v["average_fidelity"])
-        avg_success.append(v["teleport_success_count"])
-    print(f"Avg count: {np.mean(avg_count)}")
-    print(f"Avg fidelity: {np.mean(avg_fid)}")
-    print(f"Avg success: {np.mean(avg_success)}")
+    print(f"4 Node 1Km Verification Depolar 24583Hz Success Rate > 0.7: {g_7_count / len(fid_data)}")
+    print(f"4 Node 1Km Verification Depolar 24583Hz Success Rate  > 0.9: {g_9_count / len(fid_data)}")
+    print(f"4 Node 1Km Verification Depolar 24583Hz Success Rate Duration: {np.mean(d_data)}\n")
+
+
+    with open("./transportation_results/4nodes_1_qubit_verification_1.0km_6492hz_raw.json", 'r') as f:
+        data = json.load(f)
+    fid_data = data["teleport_fids"]
+    g_7_count = 0
+    g_9_count = 0
+    d_data = data["duration"]
+    for fid in fid_data:
+        if fid > 0.7:
+            g_7_count += 1
+        if fid > 0.9:
+            g_9_count += 1
+
+    print(f"4 Node 1Km Verification Depolar 6492Hz Success Rate > 0.7: {g_7_count / len(fid_data)}")
+    print(f"4 Node 1Km Verification Depolar 6492Hz Success Rate  > 0.9: {g_9_count / len(fid_data)}")
+    print(f"4 Node 1Km Verification Depolar 6492Hz Success Rate Duration: {np.mean(d_data)}\n")
