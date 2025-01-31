b4f1c9a38cc3049d77442501a5fe5ed2b970c9a0
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2025-01-31 16:43:33 -0500

    - data

--- end_to_end_experiment/cal_tool_verify.py
+++ end_to_end_experiment/cal_tool_verify.py
@@ -199,8 +199,10 @@ if __name__ == '__main__':
 
     with open("./transportation_results/e2e_4nodes_throughput_1km_verify_raw.json", 'r') as f:
         data = json.load(f)
+    g_6_counts = []
     g_7_counts = []
     g_9_counts = []
+    g_6_global_count = 0
     g_7_global_count = 0
     g_9_global_count = 0
     total_count = 0
@@ -208,6 +210,7 @@ if __name__ == '__main__':
     for value_data in data["4"].values():
         g_7_count = 0
         g_9_count = 0
+        g_6_count = 0
         fid_data = value_data["all_fids"]
         all_count.append(len(fid_data))
         for fid in fid_data:
@@ -217,13 +220,19 @@ if __name__ == '__main__':
             if fid > 0.9:
                 g_9_count += 1
                 g_9_global_count += 1
+            if fid > 0.65:
+                g_6_count += 1
+                g_6_global_count +=1
             total_count += 1
         if len(fid_data) > 0:
             g_7_counts.append(g_7_count/len(fid_data))
             g_9_counts.append(g_9_count/len(fid_data))
+            g_6_counts.append(g_6_count/len(fid_data))
 
     print(f"E2E 4 Node 1Km Verification Throughput Count: {np.mean(all_count)}")
+    print(f"E2E 4 Node 1Km Verification Throughput > 0.65: {np.mean(g_6_counts)}")
     print(f"E2E 4 Node 1Km Verification Throughput > 0.7: {np.mean(g_7_counts)}")
     print(f"E2E 4 Node 1Km Verification Throughput > 0.9: {np.mean(g_9_counts)}")
+    print(f"E2E 4 Node 1Km Verification Global > 0.7: {g_6_global_count / total_count}")
     print(f"E2E 4 Node 1Km Verification Global > 0.7: {g_7_global_count / total_count}")
     print(f"E2E 4 Node 1Km Verification Global > 0.9: {g_9_global_count / total_count}\n")
