6e241ed2776199b215d4996eca5bdc4068ac7583
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2025-01-31 15:46:51 -0500

    - data

--- hop_by_hop_experiment/cal_tool_verify.py
+++ hop_by_hop_experiment/cal_tool_verify.py
@@ -67,6 +67,24 @@ if __name__ == '__main__':
     print(f"3 Node 1Km Verification Success Rate  > 0.9: {g_9_count / len(fid_data)}")
     print(f"3 Node 1Km Verification Success Rate Duration: {np.mean(d_data)}\n")
 
+
+    with open("./transportation_results/3nodes_1_qubit_verification_0.5km_raw.json", 'r') as f:
+        data = json.load(f)
+    fid_data = data["teleport_fids"]
+    g_7_count = 0
+    g_9_count = 0
+    d_data = data["duration"]
+    for fid in fid_data:
+        if fid > 0.7:
+            g_7_count += 1
+        if fid > 0.9:
+            g_9_count += 1
+
+    print(f"3 Node 0.5Km Verification Success Rate > 0.7: {g_7_count / len(fid_data)}")
+    print(f"3 Node 0.5Km Verification Success Rate  > 0.9: {g_9_count / len(fid_data)}")
+    print(f"3 Node 0.5Km Verification Success Rate Duration: {np.mean(d_data)}\n")
+
+
     with open("./transportation_results/3nodes_1.0km_verification_throughput_raw.json", 'r') as f:
         data = json.load(f)
     g_7_counts = []
@@ -97,22 +115,6 @@ if __name__ == '__main__':
     print(f"3 Node 1Km Verification Throughput > 0.9: {np.mean(g_9_counts)}")
     print(f"3 Node 1Km Verification Global > 0.7: {g_7_global_count / total_count}")
     print(f"3 Node 1Km Verification Global > 0.9: {g_9_global_count / total_count}\n")
-
-    with open("./transportation_results/3nodes_1_qubit_verification_0.5km_raw.json", 'r') as f:
-        data = json.load(f)
-    fid_data = data["teleport_fids"]
-    g_7_count = 0
-    g_9_count = 0
-    d_data = data["duration"]
-    for fid in fid_data:
-        if fid > 0.7:
-            g_7_count += 1
-        if fid > 0.9:
-            g_9_count += 1
-
-    print(f"3 Node 0.5Km Verification Success Rate > 0.7: {g_7_count / len(fid_data)}")
-    print(f"3 Node 0.5Km Verification Success Rate  > 0.9: {g_9_count / len(fid_data)}")
-    print(f"3 Node 0.5Km Verification Success Rate Duration: {np.mean(d_data)}\n")
 
     with open("./transportation_results/4nodes_1_qubit_purification_raw.json", 'r') as f:
         data = json.load(f)
@@ -177,6 +179,23 @@ if __name__ == '__main__':
     print(f"4 Node 1Km Verification Success Rate > 0.7: {g_7_count / len(fid_data)}")
     print(f"4 Node 1Km Verification Success Rate  > 0.9: {g_9_count / len(fid_data)}")
     print(f"4 Node 1Km Verification Success Rate Duration: {np.mean(d_data)}\n")
+
+    with open("./transportation_results/4nodes_1_qubit_verification_0.5km_raw.json", 'r') as f:
+        data = json.load(f)
+    fid_data = data["teleport_fids"]
+    g_7_count = 0
+    g_9_count = 0
+    d_data = data["duration"]
+    for fid in fid_data:
+        if fid > 0.7:
+            g_7_count += 1
+        if fid > 0.9:
+            g_9_count += 1
+
+    print(f"4 Node 0.5Km Verification Success Rate > 0.7: {g_7_count / len(fid_data)}")
+    print(f"4 Node 0.5Km Verification Success Rate  > 0.9: {g_9_count / len(fid_data)}")
+    print(f"4 Node 0.5Km Verification Success Rate Duration: {np.mean(d_data)}\n")
+
 
     with open("./transportation_results/4nodes_1.0km_verification_throughput_raw.json", 'r') as f:
         data = json.load(f)
