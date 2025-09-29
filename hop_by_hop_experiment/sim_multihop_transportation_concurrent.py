c96bfab5bc9d1c8b0c553f7a58c309301062249e
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2025-09-29 14:37:46 -0400

    - add multiprocess to sim verification due to segmentation fault

--- hop_by_hop_experiment/sim_multihop_transportation_concurrent.py
+++ hop_by_hop_experiment/sim_multihop_transportation_concurrent.py
@@ -1489,10 +1489,10 @@ def run_transport_sim_worker(queue, distance, target_fid, depolar_rate, node_cou
         transport_example = None
         gc.collect()
         del transport_example
-        new_rng = np.random.RandomState()
-        if new_rng == ns.get_random_state():
-            raise ValueError("Random state is not resetting")
-        ns.set_random_state(rng=new_rng)
+        # new_rng = np.random.RandomState()
+        # if new_rng == ns.get_random_state():
+        #     raise ValueError("Random state is not resetting")
+        # ns.set_random_state(rng=new_rng)
         # Send success result
         queue.put(('success', run_index, node_data, node_data_raw))
 
@@ -1565,6 +1565,10 @@ def run_transport_sim_multiprocess(distance, target_fid, depolar_rate, node_coun
                 print(f"Process was killed by signal {-process.exitcode}")
                 # skip incrementing success_run
                 continue
+            new_rng = np.random.RandomState()
+            if new_rng == ns.get_random_state():
+                raise ValueError("Random state is not resetting")
+            ns.set_random_state(rng=new_rng)
             if result[0] == 'success':
                 _, run_index, node_data, node_data_raw = result
                 success_run += 1
