6ff2893b33a8649c2b0edf4043c2ae4737858074
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2025-10-03 16:47:43 -0400

    - bug fix

--- end_to_end_experiment/sim_end_to_end_transport.py
+++ end_to_end_experiment/sim_end_to_end_transport.py
@@ -1162,14 +1162,11 @@ def run_e2e_experiment(qubit_number=1, node_count=10, throughput_mode=False, wit
     final_data = {}
     final_data_raw = {}
     if preload:
-        try:
+        if os.path.exists(save_file) and os.path.exists(save_file_raw):
             with open(save_file, "r") as f:
                 final_data = json.load(f)
             with open(save_file_raw, "r") as f:
                 final_data_raw = json.load(f)
-        except Exception as e:
-            print(e)
-            print(f"Failed to load data from {save_file}, start fresh")
     node_data = final_data_raw
     while len(node_data["teleport_fids"]) < 1000:
         try:
