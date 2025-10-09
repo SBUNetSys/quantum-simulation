2460e5f9af315006d4ec84944b2bd6282fe9f3a2
ZhengYu Wu <zhenwu@cs.stonybrook.edu>
2025-10-09 11:03:26 -0400

    - bug fix

--- protocols/Transport.py
+++ protocols/Transport.py
@@ -85,8 +85,7 @@ class Transportation(NodeProtocol):
                 if type(protocol) is Purification:
                     await_signals.append(self.await_signal(protocol, MessageType.PURIFICATION_SUCCESS))
                 if type(protocol) is EntanglementHandlerConcurrent:
-                    await_signals.append(self.await_signal(self.entanglement_handler,
-                                                           signal_label=MessageType.ENTANGLED_SUCCESS))
+                    await_signals.append(self.await_signal(protocol, MessageType.ENTANGLED_SUCCESS))
                 else:
                     await_signals.append(self.await_signal(protocol, Signals.SUCCESS))
         # have expression to wait for ANY qubit input signal
